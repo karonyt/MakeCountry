13a5ba2f1c2311f469037ea429446ed23dbf695c
karonyt <tomatotest34@gmail.com>
2025-12-16 20:50:55 +0900

    fix

--- scripts/national_tier_level.js
+++ scripts/national_tier_level.js
@@ -104,8 +104,77 @@ export default {
                 { typeId: 'minecraft:oak_log', amount: 64 },
                 { typeId: 'minecraft:coal', amount: 64 }
             ],
-            point: 10000
+            point: 3000
         },
-
+        12: {
+            item: [
+                { typeId: 'minecraft:leaf_litter', amount: 64 * 2 },
+                { typeId: 'minecraft:gravel', amount: 64 },
+                { typeId: 'minecraft:oak_log', amount: 64 },
+                { typeId: 'minecraft:coal', amount: 64 }
+            ],
+            point: 3000
+        },
+        13: {
+            item: [
+                { typeId: 'minecraft:leaf_litter', amount: 64 * 3 },
+                { typeId: 'minecraft:gravel', amount: 64 },
+                { typeId: 'minecraft:charcoal', amount: 64 },
+                { typeId: 'minecraft:coal', amount: 64 }
+            ],
+            point: 3000
+        },
+        14: {
+            item: [
+                { typeId: 'minecraft:leaf_litter', amount: 64 * 4 },
+                { typeId: 'minecraft:gravel', amount: 96 },
+                { typeId: 'minecraft:charcoal', amount: 64 },
+                { typeId: 'minecraft:coal', amount: 64 }
+            ],
+            point: 3000
+        },
+        15: {
+            item: [
+                { typeId: 'minecraft:gravel', amount: 128 },
+                { typeId: 'minecraft:charcoal', amount: 128 },
+                { typeId: 'minecraft:coal', amount: 128 }
+            ],
+            point: 15000
+        },
+        16: {
+            item: [
+                { typeId: 'minecraft:flint', amount: 32 },
+                { typeId: 'minecraft:coal_block', amount: 16 }
+            ],
+            point: 4000
+        },
+        17: {
+            item: [
+                { typeId: 'minecraft:flint', amount: 48 },
+                { typeId: 'minecraft:coal_block', amount: 24 }
+            ],
+            point: 4000
+        },
+        18: {
+            item: [
+                { typeId: 'minecraft:flint', amount: 64 },
+                { typeId: 'minecraft:coal_block', amount: 32 }
+            ],
+            point: 4000
+        },
+        19: {
+            item: [
+                { typeId: 'minecraft:flint', amount: 96 },
+                { typeId: 'minecraft:coal_block', amount: 40 }
+            ],
+            point: 4000
+        },
+        20: {
+            item: [
+                { typeId: 'minecraft:flint', amount: 128 },
+                { typeId: 'minecraft:raw_iron_block', amount: 32 }
+            ],
+            point: 20000
+        },
     }
 };
