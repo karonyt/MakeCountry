9d345d5d968308cee2855acf1c9cb6405534c00d
karonyt <tomatotest34@gmail.com>
2026-03-12 21:44:26 +0900

    f

--- scripts/lib/app/teleport.js
+++
@@ -1,9 +0,0 @@
-import { Player } from '@minecraft/server';
-import { tpaMainForm } from '../../api/player/tpa.js';
-
-/**
- * @param {Player} player
- */
-export function TeleportApp(player) {
-    tpaMainForm(player, true)
-};
