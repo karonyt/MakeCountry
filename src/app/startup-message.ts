84bc09f529dc2f088e52b4941d4bd1f72a3153ea
karonyt <tomatotest34@gmail.com>
2026-06-18 11:53:35 +0900

    update manifest.json, startup-message.ts and tsconfig.json

--- src/app/startup-message.ts
+++ src/app/startup-message.ts
@@ -1,6 +1,6 @@
 import { world } from "@minecraft/server";
 
-const version = "ver.2.3.3-beta";
+const version = "ver.2.4.0-beta";
 
 world.afterEvents.worldLoad.subscribe(() => {
     world.sendMessage({ translate: "world.message.addon", with: [version] });
